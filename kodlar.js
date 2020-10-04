55b9bf6757583c4066864097e9f169999c6e012a
mNpoWE-r <72343369+mNpoWE-r@users.noreply.github.com>
2020-10-04 17:21:30 +0300

    Add files via upload

---
+++ kodlar.js
@@ -0,0 +1,84 @@
+onEvent("startScreen", "click", function() {
+  setScreen("whatdoyouwanttolearnabout");
+  playSound("sound://category_app/app_button_3.mp3", false);
+});
+onEvent("recycling", "click", function(){
+setScreen("recyclingsc1");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("climatechngglblwrmng", "click", function(){
+setScreen("clmtchngglblwrmng1");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("airpollution", "click", function(){
+setScreen("airpollutionsc");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("waterpollution", "click", function(){
+setScreen("waterpollutionsc1");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("soilpollution", "click", function(){
+setScreen("soilpollutionsc1");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("rnb1", "click", function(){
+setScreen("recyclingsc2");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("cgnb1", "click", function(){
+setScreen("clmtchngglblwrmng2");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("nbap1", "click", function(){
+setScreen("airpollutionsc2");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("wpnb1", "click", function(){
+setScreen("waterpollutionsc2");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("spnb1", "click", function(){
+setScreen("soilpollutionsc2");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("rnb2", "click", function(){
+setScreen("recyclingstatsandpics");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("button2", "click", function(){
+setScreen("clmtchngglblwrmngpicsstats");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("nbap2", "click", function(){
+setScreen("airpollutionchatsandpics");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("wpnb2", "click", function(){
+setScreen("waterpollutionpicsncharts");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("spnb2", "click", function(){
+setScreen("soilpollutionchartsandimages");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("btmp1", "click", function(){
+setScreen("whatdoyouwanttolearnabout");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("btmp2", "click", function(){
+setScreen("whatdoyouwanttolearnabout");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("btmp3", "click", function(){
+setScreen("whatdoyouwanttolearnabout");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("btmp4", "click", function(){
+setScreen("whatdoyouwanttolearnabout");
+playSound("sound://category_app/app_button_3.mp3");
+});
+onEvent("btmp5", "click", function(){
+setScreen("whatdoyouwanttolearnabout");
+playSound("sound://category_app/app_button_3.mp3");
+});
